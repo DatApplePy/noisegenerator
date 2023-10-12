ad1b5451fa7e86a2be9d37ea2f7ccc92ccf273be
Szohr Félix <szohrfelix@outlook.hu>
2023-10-12 14:29:51 +0200

    Controller init

--- src/main/java/hu/szohrfe/noisegenerator/Controller.java
+++ src/main/java/hu/szohrfe/noisegenerator/Controller.java
@@ -1,4 +1,11 @@
 package hu.szohrfe.noisegenerator;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class Controller {
+    private final Model model;
+    public void update() {
+        model.update();
+    }
 }
