694b32b94f3d80d1adc504fe1f99419976203627
Szohr Félix <szohrfelix@outlook.hu>
2023-10-12 14:34:11 +0200

    Build up MVC structure (attempt #1)

--- src/main/java/hu/szohrfe/noisegenerator/AppFrame.java
+++ src/main/java/hu/szohrfe/noisegenerator/AppFrame.java
@@ -1,18 +1,30 @@
 package hu.szohrfe.noisegenerator;
 
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class AppFrame extends JFrame {
 
     public AppFrame() {
         super();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        //setLayout(new SpringLayout());
 
-        Viewer viewer = new Viewer();
-        add(viewer);
-        viewer.revalidate();
-        viewer.repaint();
+        Model model = new Model(
+                (ThreadPoolExecutor) Executors.newFixedThreadPool(4),
+                new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB),
+                new PerlinNoise(512),
+                0.01, 1, -1, 1, 0, 8
+        );
+        Controller controller = new Controller(model);
+
+        View view = new View();
+        model.addObserver(view);
+        add(view);
+//        view.revalidate();
+//        view.repaint();
+        controller.update();
 
         pack();
         setVisible(true);
