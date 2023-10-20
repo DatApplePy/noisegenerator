c840d126011313e8ebf418b32693df28388a6b99
Szohr Félix <szohrfelix@outlook.hu>
2023-10-20 18:19:57 +0200

    Refactor View

--- src/main/java/hu/szohrfe/noisegenerator/view/View.java
+++ src/main/java/hu/szohrfe/noisegenerator/view/View.java
@@ -1,31 +1,55 @@
 package hu.szohrfe.noisegenerator.view;
 
 import hu.szohrfe.noisegenerator.observer.Observer;
+import lombok.Getter;
 import lombok.Setter;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+@Getter
 @Setter
 public class View extends JPanel implements Observer {
     private BufferedImage image;
+    private static final int INIT_WIDTH = 1000;
+    private static final int INIT_HEIGHT = 1000;
 
     public View() {
         super();
+        setBackground(new Color(150, 150, 150));
+        setPreferredSize(new Dimension(INIT_WIDTH, INIT_HEIGHT));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(image, 0, 0, null);
+        if(image != null) {
+            int newWidth = image.getWidth();
+            int newHeight = image.getHeight();
+
+            if (image.getWidth() > INIT_WIDTH) {
+                newWidth = INIT_WIDTH;
+                newHeight = (newWidth * image.getHeight()) / image.getWidth();
+            }
+            if (image.getHeight() > INIT_HEIGHT) {
+                newHeight = INIT_HEIGHT;
+                newWidth = (newHeight * image.getWidth()) / image.getHeight();
+            }
+
+
+            Image img = image.getScaledInstance(newWidth, newHeight, BufferedImage.SCALE_DEFAULT);
+            g.drawImage(img,
+                    getWidth()/2-img.getWidth(null)/2,
+                    getHeight()/2-img.getHeight(null)/2,
+                    null);
+        }
     }
 
     @Override
     public void update(BufferedImage image) {
         setImage(image);
-        this.revalidate();
-        this.repaint();
+        repaint();
     }
 }
