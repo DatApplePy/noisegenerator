2819df59d4a91582c2280cadedc421856930bb2d
Szohr Félix <szohrfelix@outlook.hu>
2023-10-20 18:23:23 +0200

    Implement DoubleJSlider

---
+++ src/main/java/hu/szohrfe/noisegenerator/controller/DoubleJSlider.java
@@ -0,0 +1,21 @@
+package hu.szohrfe.noisegenerator.controller;
+
+import javax.swing.*;
+
+public class DoubleJSlider extends JSlider {
+
+    private final int scale;
+
+    public DoubleJSlider(int min, int max, int value, int scale) {
+        super(min, max, value);
+        this.scale = scale;
+    }
+
+    public double getScaledValue() {
+        return (double) super.getValue() / scale;
+    }
+
+    public void setScaledValue(double value) {
+        super.setValue((int) (value * scale));
+    }
+}
