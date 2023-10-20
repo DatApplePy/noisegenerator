e589732620d713ffab1f7dae328a16e46282d5b3
Szohr Félix <szohrfelix@outlook.hu>
2023-10-20 18:18:35 +0200

    Refactor AppFrame

--- src/main/java/hu/szohrfe/noisegenerator/view/AppFrame.java
+++ src/main/java/hu/szohrfe/noisegenerator/view/AppFrame.java
@@ -5,6 +5,7 @@ import hu.szohrfe.noisegenerator.model.Model;
 import hu.szohrfe.noisegenerator.model.PerlinNoise;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -13,24 +14,21 @@ public class AppFrame extends JFrame {
 
     public AppFrame() {
         super();
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        Model model = new Model(
-                (ThreadPoolExecutor) Executors.newFixedThreadPool(4),
-                new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB),
-                new PerlinNoise(512),
-                0.01, 1, -1, 1, 0, 8
-        );
-        Controller controller = new Controller(model);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
 
         View view = new View();
-        model.addObserver(view);
-        add(view);
-//        view.revalidate();
-//        view.repaint();
-        controller.update();
+        Model model = new Model((ThreadPoolExecutor) Executors.newFixedThreadPool(4));
+        Controller controller = new Controller(model);
 
+        model.addObserver(view);
+        add(controller, BorderLayout.WEST);
+        add(view, BorderLayout.EAST);
+
+        revalidate();
+        repaint();
         pack();
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 }
