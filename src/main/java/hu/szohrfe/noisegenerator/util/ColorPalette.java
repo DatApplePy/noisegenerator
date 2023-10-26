e19d08d464a49847313efafef7a472e425cd04f7
Szohr Félix <szohrfelix@outlook.hu>
2023-10-26 14:55:38 +0200

    Added ColorPalette

---
+++ src/main/java/hu/szohrfe/noisegenerator/util/ColorPalette.java
@@ -0,0 +1,11 @@
+package hu.szohrfe.noisegenerator.util;
+
+import java.awt.*;
+
+public class ColorPalette {
+    public static final Color BLACK = new Color(0,0,0);
+    public static final Color DARK_GRAY = new Color(39, 40, 41);
+    public static final Color LIGHT_GRAY = new Color(216, 217, 218);
+    public static final Color NAVY = new Color(97, 103, 122);
+    public static final Color PINKISH_RED = new Color(255, 45, 74);
+}
