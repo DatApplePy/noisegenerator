6bade161159f98b7970c00578ad20483bf83e64b
Szohr Félix <szohrfelix@outlook.hu>
2023-10-28 16:14:37 +0200

    Add calling super.paint method

--- src/main/java/hu/szohrfe/noisegenerator/styles/MinimalNeonPanelUI.java
+++ src/main/java/hu/szohrfe/noisegenerator/styles/MinimalNeonPanelUI.java
@@ -9,6 +9,7 @@ import java.awt.*;
 public class MinimalNeonPanelUI extends BasicPanelUI {
     @Override
     public void paint(Graphics g, JComponent c) {
+        super.paint(g, c);
         c.setBackground(ColorPalette.DARK_GRAY);
     }
 }
