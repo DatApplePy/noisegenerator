ecebf0c07858e0cbbda66d66701cd44d578aedb9
Szohr Félix <szohrfelix@outlook.hu>
2023-10-28 16:13:47 +0200

    Finish MinimalNeonTextFieldUI

--- src/main/java/hu/szohrfe/noisegenerator/styles/MinimalNeonTextFieldUI.java
+++ src/main/java/hu/szohrfe/noisegenerator/styles/MinimalNeonTextFieldUI.java
@@ -1,6 +1,9 @@
 package hu.szohrfe.noisegenerator.styles;
 
+import hu.szohrfe.noisegenerator.util.ColorPalette;
+
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 import javax.swing.plaf.basic.BasicTextFieldUI;
 import java.awt.*;
 
@@ -8,6 +11,12 @@ public class MinimalNeonTextFieldUI extends BasicTextFieldUI {
     @Override
     protected void paintSafely(Graphics g) {
         super.paintSafely(g);
-        JComponent c = getComponent();
+        JTextField c = (JTextField) getComponent();
+        c.setHorizontalAlignment(JTextField.CENTER);
+        c.setBackground(ColorPalette.DARK_GRAY);
+        c.setForeground(ColorPalette.LIGHT_GRAY);
+        c.setCaretColor(ColorPalette.PINKISH_RED);
+        c.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 12));
+        c.setBorder(new MatteBorder(0,0,2,0,ColorPalette.PINKISH_RED));
     }
 }
